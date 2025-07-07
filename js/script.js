19dae4e7991908db608c8407603d9277b0acddc5
SergioGBosco <sergiogibosco@gmail.com>
2025-07-07 19:13:12 +0200

    feat: add simplified version

--- js/script.js
+++ js/script.js
@@ -1,47 +1,68 @@
 
-//creo una funzione che permette di controllare la parola "word" inserita e confrontarla con la sua versione reverse
+// //creo una funzione che permette di controllare la parola "word" inserita e confrontarla con la sua versione reverse
 
+// function reverse(word) {
+//   //Trasformo la parola inserita in un array
+
+//   let result = (``);
+//   let reverseword = [];
+
+//   // ciclo la parola appena scritta per formare un array formata dalle lettere che formano quella determinata parola come elementi
+
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     // console.log(word[i]);
+//     result += word[i];
+
+
+//     //ogni elemento lo inserisco in un array cosi da ottenere un unica parola al contrario
+//     reverseword.push(word[i])
+//   }
+//   //dato che la parola ottenuta precedentemente è sotto forma di array, dove ogni elemento viene suddiviso, unisco tutte le componenti per fare in modo di creare una singola stringa
+//   reversewordcheck = reverseword.join(``);
+
+//   console.log(result)
+
+//   console.log(reversewordcheck)
+
+//   //una volta che la parola inserita e la sua versione al contrario sono sotto forma di unica stringa metto la condizione da verificare
+
+//   // Aggiungo la condizione da verificare
+
+//   if (word === reversewordcheck) {
+//     console.log(`La Parola Scelta è Palindroma`)
+//   }
+
+//   else {
+//     console.log(`La Parola Scelta non è palindroma `)
+//   }
+
+// }
+
+// // Creo una variabile "wordcheck" che permette all'utente di inserire un parola
+
+// const wordcheck = prompt(`Inserisci la parola che vuoi Controllare `)
+
+// //Chiamo la funzione
+
+// reverse(wordcheck);
+
+
+let result = (``)
 function reverse(word) {
-  //Trasformo la parola inserita in un array 
 
-  let result = (``);
-  let reverseword = [];
-
-  // ciclo la parola appena scritta per formare un array formata dalle lettere che formano quella determinata parola come elementi
-
-  for (let i = word.length - 1; i >= 0; i--) {
-    console.log(word[i]);
+  for (i = word.length - 1; i >= 0; i--) {
     result += word[i];
-
-
-    //ogni elemento lo inserisco in un array cosi da ottenere un unica parola al contrario 
-    reverseword.push(word[i])
   }
-  //dato che la parola ottenuta precedentemente è sotto forma di array, dove ogni elemento viene suddiviso, unisco tutte le componenti per fare in modo di creare una singola stringa 
-  reversewordcheck = reverseword.join(``);
-
   console.log(result)
 
-  console.log(reversewordcheck)
-
-  //una volta che la parola inserita e la sua versione al contrario sono sotto forma di unica stringa metto la condizione da verificare 
-
-  // Aggiungo la condizione da verificare 
-
-  if (word === reversewordcheck) {
-    console.log(`La Parola Scelta è Palindroma`)
+  if (word === result) {
+    console.log(`La parola è Palindroma`)
   }
-
   else {
-    console.log(`La Parola Scelta non è palindroma `)
+    console.log(`La parola non è palindroma`)
   }
-
 }
 
-// Creo una variabile "wordcheck" che permette all'utente di inserire un parola
+const reverseword = prompt(`Scrivi una parola per vederla al contrario `)
 
-const wordcheck = prompt(`Inserisci la parola che vuoi Controllare `)
-
-//Chiamo la funzione 
-
-reverse(wordcheck);
+reverse(reverseword);
